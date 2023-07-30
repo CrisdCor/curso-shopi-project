ed15200f25eeb1d55959acaed5bc040cbb7e048c
Usuario <cristiandavid.corrales@gmail.com>
2023-07-30 12:04:19 -0500

    Primer commit tienda project

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,32 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../Home";
+import MyAccount from "../my-account";
+import MyOrder from "../my-order";
+import MyOrders from "../my-orders";
+import NotFound from "../not-found";
+import SignIn from "../sign-in";
+import Navbar from "../../Components/Navbar";
+import "./App.css";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/*", element: <NotFound /> },
+    { path: "/sign-in", element: <SignIn /> },
+  ]);
+  return routes;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
+  );
+};
+
+export default App;
