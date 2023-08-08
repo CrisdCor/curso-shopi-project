869883080cc0d6b31ebb0976ed7448bd741bd6e9
Usuario <cristiandavid.corrales@gmail.com>
2023-08-07 21:01:36 -0500

    Se crea funcionalidad de agregar productos al carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
+import OrderCard from "../OrderCard";
 import "./styles.css";
 
 const CheckoutSideMenu = () => {
@@ -20,6 +21,16 @@ const CheckoutSideMenu = () => {
         >
           <XMarkIcon className="h-6 w-6"></XMarkIcon>
         </div>
+      </div>
+      <div className="px-6 overflow-scroll">
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageURL={product.images}
+            price={product.price}
+          />
+        ))}
       </div>
     </aside>
   );
---
+++ src/Components/OrderCard/index.jsx
@@ -0,0 +1,31 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+const OrderCard = (props) => {
+  const { title, imageURL, price } = props;
+
+  return (
+    <div className="flex justify-between items-center mb-3">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-20">
+          <img
+            className="w-full h-full rounded-lg object-cover"
+            src={imageURL}
+            alt={title}
+          />
+        </figure>
+        <p className="text-sm font-light">{title}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium">{price}</p>
+        <div
+          className="cursor-pointer"
+          onClick={() => context.closeCheckoutSideMenu()}
+        >
+          <XMarkIcon className="h-6 w-6"></XMarkIcon>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;
